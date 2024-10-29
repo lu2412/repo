91f106a8978cd389c04275ff8a90f87ad330396c
liym5238 <https: //liym5238@github.com>
2024-10-29 20:51:04 +0800

    xxx

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 切换到包含Packages文件的目录
+cd /var/jb/var/mobile/liym
+
+# 检查Packages文件是否存在，如果存在，则压缩为.gz格式
+if [ -f Packages ]; then
+    gzip Packages
+fi
+
+# 执行dpkg-scanpackages命令，生成Packages文件
+dpkg-scanpackages . /dev/null > Packages
