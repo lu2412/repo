b58c5d59d4401e950999598aa4fdc3dad1da5b6e
lu2412 <liym5238@gmail.com>
2026-08-26 20:40:21 +0800

    更新 Packages 文件

--- 一键替换json链接.sh
+++ 一键替换json链接.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 要替换的旧字符串
-old_string="liym5238.github.io/liym"
+old_string="https://qr.alipay.com/fkx19423rvx2js7rku0hk9f/"
 
 # 要替换成的新字符串
-new_string="lu2412.github.io/repo"
+new_string="https://qr.alipay.com/fkx19423rvx2js7rku0hk9f"
 
 # 查找当前目录下所有的 JSON 文件，并替换字符串
 find . -type f -name "*.json" -print0 | while IFS= read -r -d '' file; do
